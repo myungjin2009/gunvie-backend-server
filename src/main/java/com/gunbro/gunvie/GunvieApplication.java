6912bda284a4f921a8fd9654c5a37174108afa7a
Kim Myeongjin <myungjin2009@naver.com>
2023-01-26 21:38:37 +0900

    Feat(Project) : 프로젝트 기본 설정

---
+++ src/main/java/com/gunbro/gunvie/GunvieApplication.java
@@ -0,0 +1,13 @@
+package com.gunbro.gunvie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunvieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunvieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gunbro/gunvie/GunvieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunbro.gunvie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunvieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
