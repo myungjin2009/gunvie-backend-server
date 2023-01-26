a1a9884a6fd76bd3529e94375f4e3bfe3853b96f
Kim Myeongjin <myungjin2009@naver.com>
2023-01-26 23:37:04 +0900

    Feat : SpringSecurity + MySQL Dependency 추가

--- src/main/java/com/gunbro/gunvie/GunvieApplication.java
+++ src/main/java/com/gunbro/gunvie/GunvieApplication.java
@@ -2,8 +2,9 @@ package com.gunbro.gunvie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= SecurityAutoConfiguration.class)
 public class GunvieApplication {
 
 	public static void main(String[] args) {
